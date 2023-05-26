fd2e82b890a3e7d1e7cb42762fc196c4ae4cea5b
enigma85 <enigmasailor85@gmail.com>
2023-05-26 07:57:46 -0500

    Update curl_install.sh

--- curl_install.sh
+++ curl_install.sh
@@ -19,7 +19,7 @@ service_install_dir="/etc/systemd/system"
 script_install_dir="/home/nazo/.local/share/scawp/SDMED"
 
 device_name="$(uname --nodename)"
-user="$(id -u deck)"
+user="$(id -u nazo)"
 
 if [ "$device_name" != "steamdeck" ] || [ "$user" != "1000" ]; then
   zenity --question --width=400 \
